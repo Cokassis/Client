109e8a911ed1a671ddcde3a6ab31c3f51f8069e9
sysuye <troubleiloveyou@foxmail.com>
2017-05-23 14:40:36 +0800

    增加food.views.search的注释

--- food/views.py
+++ food/views.py
@@ -7,12 +7,21 @@ from supplier.models import Supplier, MatSellInfo
 
 
 def search(request):
+    # 判断来自客户的请求是否通过GET发送
     if request.method == 'GET':
+        #获取GET请求中附带的查询关键字字符串,也就是search_str
         search_str = request.GET['search_str']
+        #通过查询字符串在数据库查找相关的菜品，例如用户查找的的关键字为’番茄‘，将会根据数据库name字段查询到
+        #相关的菜品，如’番茄炒蛋‘，’番茄XXX','XXX番茄XXX‘等，下面语句中的'name__contains= search_str’
+        #意思是‘名字（name)包含（contains）’，为英文相应单词的直译.
         dishes = Dish.objects.filter(name__contains= search_str)
+        #下句的意思同上，查询食材
         materials = Material.objects.filter(name__contains= search_str)
+        #初始化一个空的菜品列表，用于保存需要的菜品信息
         dishes_list = []
+        #遍历查询到的dishes
         for adish in dishes:
+            #生成一个dish的字典
             dish_dict = {
                 'id': adish.id,
                 'name': adish.name,
@@ -20,7 +29,9 @@ def search(request):
                 'discount': adish.discount,
                 'like': adish.like
             }
+            #把上面生成的单个菜品的字典加入菜品列表中
             dishes_list.append(dish_dict)
+        # 初始化一个空的食材列表，用于保存需要的食材信息，原理同查询菜品
         material_list = []
         for amaterial in materials:
             material_dict = {
@@ -29,10 +40,12 @@ def search(request):
                 'breed': amaterial.breed
             }
             material_list.append(material_dict)
+        #把上述生成的菜品、食材列表整合成字典形式的结果，用于返回请求所需的JSON
         result = {
             'dishes': dishes_list,
             'materials': material_list
         }
+        #返回
         return JsonResponse(result)
 
 
